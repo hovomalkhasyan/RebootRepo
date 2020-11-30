cde4dfbd83bc3d25b64e8b14d8f297d24b12e27b
Hovo Malkhasyan <macbook@MacBook-Air-Hovo.local>
2020-11-30 16:49:46 +0400

    add splashController

--- Reboot/SceneDelegate.swift
+++ Reboot/SceneDelegate.swift
@@ -11,7 +11,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions:     UIScene.ConnectionOptions) {
             if let windowScene = scene as? UIWindowScene {
                 let window = UIWindow(windowScene: windowScene)
                 let storyboard: UIStoryboard = UIStoryboard(name: "Splash", bundle: nil)
